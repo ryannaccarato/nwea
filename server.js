1a917bc987341e0322156d184d99e95a4994ef94
Ryan Naccarato <ryannaccarato@outlook.com>
2017-03-18 13:20:17 -0700

    Finished POST functionality.

--- server.js
+++ server.js
@@ -1,13 +1,48 @@
 var express = require('express');
 var sqlite = require('sqlite3').verbose();
+var bodyParser = require('body-parser');
+
 var server = express();
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 var db = new sqlite.Database('blog.db');
 
 server.get('/posts', function(request, response) {
-    db.each('SELECT * FROM posts', function(error, rows) {
-        if (error) console.log(error);
-        response.send(rows);
+    db.all('SELECT * FROM posts', function(error, rows) {
+        if (error) { 
+            console.log(error);
+            response.send('Could not access the blog.');            
+        }
+        else { 
+            console.log('Blog database successfully queried.');
+            response.send(rows);            
+        }
     });
+});
+
+server.post('/post', function(request, response) {
+    var title = request.body.title;
+    var body = request.body.body;
+    if (title == null || body == null) 
+        response.send('Post received, but data was missing. Try using url-encoding for your POST.');
+    else {
+        try {
+            var insert = db.prepare('INSERT INTO posts(title,body) VALUES($title,$body)');
+            insert.run({ $title: title, $body: body }, function(error, row) {
+                if (error) {
+                    console.log(error);
+                    response.send('POST could not be saved.');
+                }
+                else {
+                    console.log('New blog post added to the database.');
+                    response.send('POST completed successfully.');
+                }
+            });
+        } catch (error) { 
+            console.log(error);
+            response.send('POST could not be saved.');
+        }
+    }
 });
 
 server.listen(3000);
