89d3c14ee759ed8ee3a23518ce41055d0aaaed5a
Ryan Naccarato <ryannaccarato@outlook.com>
2017-03-18 12:18:20 -0700

    Completed GET functionality.

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var sqlite = require('sqlite3').verbose();
 var server = express();
+var db = new sqlite.Database('blog.db');
 
-server.get('/', function(request, response) {
-    response.send('server running...');
-})
+server.get('/posts', function(request, response) {
+    db.each('SELECT * FROM posts', function(error, rows) {
+        if (error) console.log(error);
+        response.send(rows);
+    });
+});
 
 server.listen(3000);
-console.log('Express server successfully setup and listening on port 3000.');
+console.log('\nExpress server successfully setup and listening on port 3000.');
